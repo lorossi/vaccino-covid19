4fa4266c119ae60cf5e58b0004e8ab1fd600f39f
vaccini-covid-update <vaccini-covid@lorenzoros.si>
2021-01-06 17:00:07 +0100

    updated data

--- assets/vaccini.js
+++ assets/vaccini.js
@@ -277,7 +277,7 @@ let vaccini = {
             "nuovi_vaccinati": 64
         }
     ],
-    "script_timestamp": "2021-01-06T16:00:05.294530",
-    "last_updated": "2021-01-06 ore 16:00",
-    "last_data_update": "2021-01-06T14:27:31.143Z"
+    "script_timestamp": "2021-01-06T17:00:05.027673",
+    "last_updated": "2021-01-06 ore 17:00",
+    "last_data_update": "2021-01-06T15:27:21.738Z"
 };
