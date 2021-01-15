0304d1a933789ec13d75297168f10027b667f2f4
vaccini-covid-update <vaccini-covid@lorenzoros.si>
2021-01-15 14:30:17 +0100

    updated data

--- assets/vaccini.js
+++ assets/vaccini.js
@@ -356,7 +356,7 @@ let vaccini = {
       "percentuale_nuovi_vaccinati": 1.6521078617546525
     }
   ],
-  "script_timestamp": "2021-01-15T14:00:14.211782",
-  "last_updated": "2021-01-15 ore 14:00",
+  "script_timestamp": "2021-01-15T14:30:14.919466",
+  "last_updated": "2021-01-15 ore 14:30",
   "last_data_update": "2021-01-15T12:45:00.483Z"
 };
