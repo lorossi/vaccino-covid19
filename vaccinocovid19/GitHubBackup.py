803aa966ef64cb5d6cbc7a2533cb54da0e3958b0
lorossi <mail@lorenzoros.si>
2021-01-18 17:41:36 +0100

    another try

--- vaccinocovid19/GitHubBackup.py
+++ vaccinocovid19/GitHubBackup.py
@@ -1,20 +1,33 @@
+import logging
+
 class Backup:
     def __init__(self):
+        logfile = "backup.log"
+        logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
+                            level=logging.INFO, filename=logfile,
+                            filemode="a")
         return
 
     def backup(self):
         from git import Repo
+        logging.info("Started backup process")
         # now push all to to github
         # repo folder is parent
         repo = Repo(".", search_parent_directories=True)
         # add all modified files
         repo.git.add("-A")
+        logging.info("Added files")
         repo.index.commit("updated data")
+        logging.info("Commit created")
         # pull and push
-        repo.git.pull()
+        logging.info("Repo pushed")
         repo.git.push()
+        logging.info("Repo pulled")
+        repo.git.pull()
 
 
 if __name__ == "__main__":
     b = Backup()
-    b.backup
+    logging.info("Item created")
+    b.backup()
+    logging.info("Backup completed")
--- vaccinocovid19/vaccinocovid19.py
+++ vaccinocovid19/vaccinocovid19.py
@@ -21,7 +21,7 @@ def main():
     scheduler = BackgroundScheduler()
     scheduler.start()
     scheduler.add_job(scrape_data, trigger="cron", minute="*/15")
-    scheduler.add_job(push_to_github, trigger="cron", minute="38", hour="17")
+    scheduler.add_job(push_to_github, trigger="cron", minute="50", hour="17")
     scheduler.add_job(scrape_history, trigger="cron", minute="5", hour="0")
     scheduler.add_job(scrape_colors, trigger="cron", minute="10", hour="0")
     s.loadData()
@@ -45,7 +45,7 @@ def scrape_colors():
 
 
 def push_to_github():
-    subprocess.run(["sh", "backup.sh", ">", "outfile"])
+    subprocess.run(["sh", "backup.sh"])
 
 
 # error 500 page
