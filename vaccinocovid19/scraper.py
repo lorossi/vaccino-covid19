1ad0a85da742c6dd0bccf85ba41556e388aaccc9
lorossi <contact@lorenzoros.si>
2021-03-14 14:14:58 +0100

    added colors for quick parsing

--- vaccinocovid19/scraper.py
+++ vaccinocovid19/scraper.py
@@ -1065,7 +1065,7 @@ class Scraper:
         self.loadColorsMap()
         return_dict = {}
         for i, c in enumerate(self._colors_map):
-            hex_color = int(self._colors_map[c]['rgb'][1:], 16)
+            hex_color = int(self._colors_map[c]['arduino_rgb'][1:], 16)
             return_dict[i] = hex_color
         return return_dict
 
